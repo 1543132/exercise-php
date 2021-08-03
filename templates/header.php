28295a9336dd0fa89e56d4d200f82af6471ad391
Shirley <achouinard@pardesign.net>
2021-08-03 17:10:07 -0400

    SESSION & COOKIE exercise

--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,20 @@
+<?php 
+
+	session_start();
+
+	$name = $_SESSION['name'] ?? 'Guest';
+
+	$gender = $_COOKIE['gender'] ?? 'Unknown';
+?>
+
 <head>
 	<title>Ninja Pizza</title>
 	<!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <style type="text/css">
-	  .brand{
-	  	background: #cbb09c !important;
-	  }
+	.brand{
+	background: #cbb09c !important;
+	}
   	.brand-text{
   		color: #cbb09c !important;
   	}
@@ -18,10 +27,12 @@
 </head>
 <body class="grey lighten-4">
 	<nav class="white z-depth-0">
-    <div class="container">
-      <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
-      <ul id="nav-mobile" class="right hide-on-small-and-down">
-        <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
-      </ul>
-    </div>
+	<div class="container">
+	  <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
+	  <ul id="nav-mobile" class="right hide-on-small-and-down">
+		<li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
+		<li class="grey-text">(<?php echo htmlspecialchars($gender); ?>)</li>
+		<li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
+	  </ul>
+	</div>
   </nav>
